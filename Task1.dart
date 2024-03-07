34afc970a2afa8140ef5e7e8f3a4395e369a1db1
Clide Odhiambo <160271403+clide98@users.noreply.github.com>
2024-03-07 16:43:57 +0300

    Add files via upload

---
+++ Task1.dart
@@ -0,0 +1,10 @@
+void main() {
+  var number = 30;
+  if (number > 10) {
+    print("Your number is greater than 10");
+  } else if (number < 10) {
+    print('Your number is less than 10');
+  } else {
+    print('Your number is equal to 10');
+  }
+}
